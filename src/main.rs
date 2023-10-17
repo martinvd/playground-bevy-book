2bffb114098d64bdebd34bf8a96fb660aa0a5f27
Martin van Driel <martinvd@gmail.com>
2023-10-17 19:07:32 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
 }
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
 
 fn hello_world() {
     println!("Hello World!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Jack Jackson".to_string())));
+    commands.spawn((Person, Name("Joe Joeson".to_string())));
+    commands.spawn((Person, Name("John Johnson".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0)
+    }
 }
